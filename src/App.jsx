017d418e52069d1cae13bb1e3758e9aec2bfd039
sungchulsckim <sungchul.sckim@outlook.com>
2024-08-23 20:14:55 -0700

    Imported useEffect, axios and fetched data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.scss'
-import videoData from './data/video-details.json'
+// import videoData from './data/video-details.json'
 import Header from './components/Header/Header'
 import Comments from './components/Comments/Comments'
 import VideoList from './components/VideoList/VideoList'
 import SelectedVideo from './components/SelectedVideo/SelectedVideo'
 import CommentList from './components/CommentList/CommentList'
 import SelectedVideoScreen from './components/SelectedVideoScreen/SelectedVideoScreen'
+import { API_KEY, API_URL, endpointVideo } from './pages/util.js'
+import axios from 'axios'
 
 function App() {
-  const [selectedVideo, setSelectedVideo] = useState(videoData[0])
-  const filteredVideoList = videoData.filter(video => video.id !== selectedVideo.id)
+  const [videos, setVideos] = useState([])
+  // const [selectedVideo, setSelectedVideo] = useState(videos[0])
+  const [selectedVideo, setSelectedVideo] = useState()
+  const filteredVideoList = videos.filter(video => video.id !== selectedVideo.id)
+
+  const getVideos = async () => {
+    const results = await axios.get (`${API_URL}${endpointVideo}${API_KEY}`)
+    const videos = results.data
+    setVideos(videos)
+    setSelectedVideo(videos[0])
+  }
+
+  useEffect( () => {
+    getVideos ()
+  }, [])
+
+
+
+
+  if (selectedVideo === undefined) {
+    return (
+      <>
+        <Header />
+        <main>Loading...</main>
+      </>
+    )
+  }
+
+
+
 
   return (
     <>
@@ -20,8 +50,8 @@ function App() {
         <div className="main__container">
           <div className="main__left-wrapper">
             <SelectedVideo video={selectedVideo} />
-            <Comments comments={selectedVideo.comments} />
-            <CommentList comments={selectedVideo.comments} />
+            {/* <Comments comments={selectedVideo.comments} /> */}
+            {/* <CommentList comments={selectedVideo.comments} /> */}
           </div>
           <div className="main__right-wrapper">
             <VideoList video={filteredVideoList} setSelectedVideo={setSelectedVideo} />
---
+++ src/pages/util.js
@@ -0,0 +1,3 @@
+export const API_KEY = "?api_key=4350c558-7843-49e4-aa07-792f40f2f3a6"
+export const API_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com"
+export const endpointVideo = "/videos"

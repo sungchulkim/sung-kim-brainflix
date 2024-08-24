79edd30e4b68e40a836cad9096d2480f19a472b5
sungchulsckim <sungchul.sckim@outlook.com>
2024-08-23 22:18:26 -0700

    Added useParams to find the selected video

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,54 @@
-import { useState, useEffect } from 'react'
 import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+// import { useState, useEffect } from 'react'
 // import videoData from './data/video-details.json'
-import Header from './components/Header/Header'
-import Comments from './components/Comments/Comments'
-import VideoList from './components/VideoList/VideoList'
-import SelectedVideo from './components/SelectedVideo/SelectedVideo'
-import CommentList from './components/CommentList/CommentList'
-import SelectedVideoScreen from './components/SelectedVideoScreen/SelectedVideoScreen'
-import { API_KEY, API_URL, endpointVideo } from './pages/util.js'
-import axios from 'axios'
+// import Header from './components/Header/Header'
+// import Comments from './components/Comments/Comments'
+// import VideoList from './components/VideoList/VideoList'
+// import SelectedVideo from './components/SelectedVideo/SelectedVideo'
+// import CommentList from './components/CommentList/CommentList'
+// import SelectedVideoScreen from './components/SelectedVideoScreen/SelectedVideoScreen'
+// import { API_KEY, API_URL, endpointVideo } from './pages/util.js'
+// import axios from 'axios'
 
 function App() {
-  const [videos, setVideos] = useState([])
+
+  
+  // const [videos, setVideos] = useState([])
   // const [selectedVideo, setSelectedVideo] = useState(videos[0])
-  const [selectedVideo, setSelectedVideo] = useState()
-  const filteredVideoList = videos.filter(video => video.id !== selectedVideo.id)
+  // const [selectedVideo, setSelectedVideo] = useState()
+  // const filteredVideoList = videos.filter(video => video.id !== selectedVideo.id)
 
-  const getVideos = async () => {
-    const results = await axios.get (`${API_URL}${endpointVideo}${API_KEY}`)
-    const videos = results.data
-    setVideos(videos)
-    setSelectedVideo(videos[0])
-  }
+  // const getVideos = async () => {
+  //   const results = await axios.get (`${API_URL}${endpointVideo}${API_KEY}`)
+  //   const videos = results.data
+  //   setVideos(videos)
+  //   setSelectedVideo(videos[0])
+  // }
 
-  useEffect( () => {
-    getVideos ()
-  }, [])
+  // useEffect( () => {
+  //   getVideos ()
+  // }, [])
 
-
-
-
-  if (selectedVideo === undefined) {
-    return (
-      <>
-        <Header />
-        <main>Loading...</main>
-      </>
-    )
-  }
-
-
-
+  // if (selectedVideo === undefined) {
+  //   return (
+  //     <>
+  //       <Header />
+  //       <main>Loading...</main>
+  //     </>
+  //   )
+  // }
 
   return (
     <>
-      <Header />
-      <main className="main">
-        <SelectedVideoScreen video={selectedVideo} />
-        <div className="main__container">
-          <div className="main__left-wrapper">
-            <SelectedVideo video={selectedVideo} />
-            {/* <Comments comments={selectedVideo.comments} /> */}
-            {/* <CommentList comments={selectedVideo.comments} /> */}
-          </div>
-          <div className="main__right-wrapper">
-            <VideoList video={filteredVideoList} setSelectedVideo={setSelectedVideo} />
-          </div>
-        </div>
-      </main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/videos/:age" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }

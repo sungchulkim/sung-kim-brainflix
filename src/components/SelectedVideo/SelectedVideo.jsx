e7bbbdc18738f5f7a80737ae7581be0d603cd3ff
sungchulkim <37005088+sungchulkim@users.noreply.github.com>
2024-09-01 16:32:19 -0700

    Reflect the feedback. Adjust the font-size, input and button height, padding and margin

--- src/components/SelectedVideo/SelectedVideo.jsx
+++ src/components/SelectedVideo/SelectedVideo.jsx
@@ -14,22 +14,22 @@ const SelectedVideo = ({ video }) => {
                     <div className="selected-video__details-container">
 
                         <div className='selected-video__inner-details-left-container'>
-                            <h3 className='selected-video__by demi'>By {video.channel}</h3>
-                            <h3 className='selected-video__date normal'>{new Date(video.timestamp).toLocaleDateString()}</h3>
+                            <h3 className='selected-video__by bold'>By {video.channel}</h3>
+                            <h4 className='selected-video__date normal'>{new Date(video.timestamp).toLocaleDateString()}</h4>
                         </div>
                         <div className='selected-video__inner-details-right-container'>
                             <div className="selected-video__inner-details-left-wrapper">
                                 <img className='selected-video__icons' src={viewIconViews} alt="view icons" />
-                                <h3 className='selected-video__view normal'>{video.views}</h3>
+                                <h4 className='selected-video__view normal'>{video.views}</h4>
                             </div>
                             <div className="selected-video__inner-details-right-wrapper">
                                 <img className='selected-video__icons' src={viewIconLikes} alt="view icons" />
-                                <h3 className='selected-video__like normal'>{video.likes}</h3>
+                                <h4 className='selected-video__like normal'>{video.likes}</h4>
                             </div>
-
                         </div>
 
                     </div>
+                    
                     <hr className="hr" />
                     <p className="selected-video__description line-height">{video.description}</p>
                 </div>
